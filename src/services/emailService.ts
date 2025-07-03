65402affdd5571f1e3f49c345a4b26201d5da91c
ArchanaGundla <86965852+ArchanaGundla@users.noreply.github.com>
2025-07-03 23:32:09 +0530

    Update emailService.ts

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -2,7 +2,7 @@
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
   'https://temp-mail-server.vercel.app/api';
-
+console.log(import.meta.env.VITE_API_BASE_URL)
 export interface EmailMessage {
   id: string;
   from: string;
