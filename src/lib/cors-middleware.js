bf2c205987a9b24d08320db356df9fd5ce327933
ArchanaGundla <86965852+ArchanaGundla@users.noreply.github.com>
2025-07-04 22:39:52 +0530

    Create cors-middleware.js

---
+++ src/lib/cors-middleware.js
@@ -0,0 +1,11 @@
+import Cors from 'cors';
+import initMiddleware from './init-middleware';
+
+const cors = initMiddleware(
+  Cors({
+    origin: 'https://temp-mail-olive-ten.vercel.app',
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+);
+
+export default cors;
